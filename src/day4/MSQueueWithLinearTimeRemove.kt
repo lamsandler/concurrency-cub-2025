c7cb03afeea5b12240cddd2a4ad68ecb19b368b5
Stanislav.Sandler <ssandler23@gmail.com>
2025-11-30 16:03:28 +0100

    Homework 4 done

--- src/day4/MSQueueWithLinearTimeRemove.kt
+++ src/day4/MSQueueWithLinearTimeRemove.kt
@@ -2,7 +2,6 @@
 
 package day4
 
-import day4.*
 import java.util.concurrent.atomic.*
 
 class MSQueueWithLinearTimeRemove<E> : QueueWithRemove<E> {
@@ -16,18 +15,30 @@ class MSQueueWithLinearTimeRemove<E> : QueueWithRemove<E> {
     }
 
     override fun enqueue(element: E) {
-        // TODO: When adding a new node, check whether
-        // TODO: the previous tail is logically removed.
-        // TODO: If so, remove it physically from the linked list.
-        TODO("Implement me!")
+        while(true) {
+            val currentTail = tail.get()
+            val newNode = Node(element)
+            if (currentTail.next.compareAndSet(null, newNode)) {
+                tail.compareAndSet(currentTail, newNode)
+                if (currentTail.extractedOrRemoved) {
+                    currentTail.remove()
+                }
+                return
+            } else {
+                tail.compareAndSet(currentTail, currentTail.next.get())
+            }
+        }
     }
 
     override fun dequeue(): E? {
-        // TODO: After moving the `head` pointer forward,
-        // TODO: mark the node that contains the extracting
-        // TODO: element as "extracted or removed", restarting
-        // TODO: the operation if this node has already been removed.
-        TODO("Implement me!")
+        while(true) {
+            val currentHead = head.get()
+            val currentHeadNext = currentHead.next.get() ?: return null
+            if (head.compareAndSet(currentHead, currentHeadNext)) {
+                if (!currentHeadNext.markExtractedOrRemoved()) continue
+                return currentHeadNext.element
+            }
+        }
     }
 
     override fun remove(element: E): Boolean {
@@ -63,16 +74,11 @@ class MSQueueWithLinearTimeRemove<E> : QueueWithRemove<E> {
         }
     }
 
-    // TODO: Node is an inner class for accessing `head` in `remove()`
     private inner class Node(
         var element: E?
     ) {
         val next = AtomicReference<Node?>(null)
 
-        /**
-         * TODO: Both [dequeue] and [remove] should mark
-         * TODO: nodes as "extracted or removed".
-         */
         private val _extractedOrRemoved = AtomicBoolean(false)
         val extractedOrRemoved
             get() =
@@ -81,6 +87,15 @@ class MSQueueWithLinearTimeRemove<E> : QueueWithRemove<E> {
         fun markExtractedOrRemoved(): Boolean =
             _extractedOrRemoved.compareAndSet(false, true)
 
+        private fun findPrevious(): Node? {
+            var current = head.get()
+            while(true) {
+                val next = current.next.get() ?: return null
+                if (next === this) return current
+                current = next
+            }
+        }
+
         /**
          * Removes this node from the queue structure.
          * Returns `true` if this node was successfully
@@ -88,17 +103,12 @@ class MSQueueWithLinearTimeRemove<E> : QueueWithRemove<E> {
          * removed by [remove] or extracted by [dequeue].
          */
         fun remove(): Boolean {
-            // TODO: The removal procedure is split into two phases.
-            // TODO: First, you need to mark the node as "extracted or removed".
-            // TODO: On success, this node is logically removed, and the
-            // TODO: operation should return `true` at the end.
-            // TODO: In the second phase, the node should be removed
-            // TODO: physically, updating the `next` field of the previous
-            // TODO: node to `this.next.value`.
-            // TODO: Do not remove `head` and `tail` physically to make
-            // TODO: the algorithm simpler. In case a tail node is logically removed,
-            // TODO: it will be removed physically by `enqueue(..)`.
-            TODO("Implement me!")
+            val isRemoved = markExtractedOrRemoved()
+            val currentPrevious = findPrevious()
+            val currentNext = next.get() ?: return isRemoved
+            currentPrevious?.next?.set(currentNext)
+            if (currentNext.extractedOrRemoved) currentNext.remove()
+            return isRemoved
         }
     }
 }
